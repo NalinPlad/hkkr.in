5818dfcce0de8d94169fd78515561c35e0df3795
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-08-22 13:30:46 -0700

    Prepare for heroku init

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const morgan = require('morgan')
+const url = require('node:url')
 const express = require('express');
 const app = express();
 const https = require('https');
@@ -12,7 +14,12 @@ const UNDERLINE = "\x1b[4m"
 const DIM = "\x1b[2m"
 
 const MAGENTA = "\x1b[38;5;50m"
-const BGTITLE = ""
+const URL_C = "\x1b[38;5;103m"
+const TITLE = "\x1b[38;5;253m"
+const TIME = "\x1b[38;5;240m"
+
+const HIGHLIGHT = "\x1b[48;5;220m\x1b[38;5;232m"
+const BLACK_HIGHLIGHT = "\x1b[48;5;232m"
 
 // Vote colors
 const L1 = "\x1b[38;5;214m"
@@ -23,13 +30,48 @@ const L5 = "\x1b[38;5;112m"
 const L6 = "\x1b[38;5;40m"
 
 // Max character length of title before cropping ...
-const MAXLEN = 80 
+const MAXLEN = 70 
 
-app.get('/', async function(req, res){
-  const URL_top = "https://hacker-news.firebaseio.com/v0/topstories.json"
-  const URL_story = "https://hacker-news.firebaseio.com/v0/item/"
-  let settings = { method: "Get" };
+// Cache for 30 seconds
+let setCache = function (req, res, next) {
+  const period = 30 
+
+  // you only want to cache for GET requests
+  if (req.method == 'GET') {
+    res.set('Cache-control', `public, max-age=${period}`)
+  } else {
+    // for the other requests set strict no caching parameters
+    res.set('Cache-control', `no-store`)
+  }
   
+  next()
+}
+
+app.use(morgan('dev'))
+app.use(setCache)
+
+const URL_top = "https://hacker-news.firebaseio.com/v0/topstories.json"
+const URL_story = "https://hacker-news.firebaseio.com/v0/item/"
+const URL_comments = "https://news.ycombinator.com/item?id="
+let settings = { method: "Get" };
+
+app.get('/:id?', async function(req, res){
+  // Comments
+  if(req.params.id != undefined){
+    
+    fetch(URL_story+req.params.id+".json")
+      .then(res => res.json())
+      .then((json) => {
+        if(json){
+          res.redirect(URL_comments + req.params.id)
+        } else {
+          res.status(404).send("Invalid ID. You can -go home- or -continue anyways-")
+        }
+      })
+  
+  } else { 
+
+  // Home
   fetch(URL_top, settings)
     .then(res => res.json())
     .then((json) => {
@@ -46,7 +88,7 @@ app.get('/', async function(req, res){
         // Header
         const head = `${BOLD+MAGENTA}HKKR.IN/CURLME${RESET} · ${now.toUTCString()+RESET}\n` 
         
-        const linebreak = "~~~~~~~~~~~~~~\n"
+        const linebreak = "\n"
 
         let out = [head,linebreak]
         
@@ -68,20 +110,55 @@ app.get('/', async function(req, res){
           if(story.title.length > MAXLEN){
             t = story.title.substring(0, MAXLEN - 3) + "..."
           }
-          // Title
-          out.push(`${BOLD+(ind+1)+RESET}. ${BOLD+t+RESET}${RESET}\n`)
-          // Score
-          out.push(`    ▴${sc+s+RESET+" ".repeat(4 - s.toString().length)}➤hkkr.in/${story.id}${RESET}\n`)
+          // Title                          Ternary ♡
+          out.push(`${BOLD+(ind+1)}. ${(ind+1 > 9 ? "" : " ") + TITLE+t+RESET}\n`)
+          
+          // Score & urls
+          let domain = ""
+          if (story.url != undefined) domain = " (" + new URL(story.url).hostname.replace("www.","") + ")"
+          
+          out.push(`${BOLD}➥${RESET}    ▴${sc+s+RESET+" ".repeat(4 - s.toString().length)}➤ hkkr.in/${story.id}${RESET+URL_C}${domain+RESET}\n`)
 
         })
         // Footer
-        out.push()
+        out.push(`${HIGHLIGHT}`)
         res.send(out.join(""))
       })
 
     });
+  }
+});
+
+app.get('/c/:id', async function(req, res){
+res.send(req.params.id)
+
+});
+
+app.get('/CURLME', async function(req, res){
+  res.send(`
+${BOLD}HKKR.IN, the best way to get your daily Hacker News dosage
+${RESET}-
+hkkr.in can be used in the terminal, using 'curl', which when run with no parameters
+  
+  ${HIGHLIGHT}curl hkkr.in${RESET}
+  
+will display the top 10 HN posts
+
+A single entry will look like this:
+  
+  ${BOLD}4.  PayPal phishing scam uses invoices sent via PayPal
+  ${BOLD}➥${RESET}    ▴${L5}144${RESET} ➤ hkkr.in/32511086
+
+You can use the link above, ${BOLD}hkkr.in/32511086${RESET} to view to discussion in the terminal
+
+  ${HIGHLIGHT}curl hkkr.in/32511086${RESET}
+
+You can also view the discussion on Hacker News by opening the same link in a browser
+
+
+
+  `)
 });
 
 app.listen(PORT);
-console.log(`${MAGENTA+BOLD}Listening on ${PORT+RESET}`)
-
+console.log(`${TITLE+BOLD}Listening on ${PORT+RESET}\n---\n`)
