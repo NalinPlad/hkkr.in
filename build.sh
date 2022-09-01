cf2202009b79bcbd597eb28752cae293c628856b
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-08-31 21:37:14 -0700

    edit gitignore, and update build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Removing old builds..'
-rm AWS-BUILD.zip
+rm BUILD.zip
 echo 'Zipping..'
-zip -r AWS-BUILD.zip src/ package.json -x "*/\.*" -q
+zip -r BUILD.zip src/ package.json -x "*/\.*" -q
 echo 'done => AWS-BUILD.zip'
