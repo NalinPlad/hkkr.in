080c47aae0fb47583e49aa7e7e25687b687a2072
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-09-09 12:16:03 -0700

    removed deps

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ async function GetUrl(id){
 app.get('/:q?', async function(req, res){
   const opts = parser.parse(req.params.q)
   
-  if(req.useragent.browser != "curl" && opts.u === false) {
+  if(req.useragent.browser != "curl" && opts.u === false && opts.id === 0) {
     return res.redirect(302, "https://github.com/NalinPlad/hkkr.in");
   }
   if(opts == "curlme"){
